7a9745affaaaabc7f47bbff614503ca0d1144b6b
lamsasa <dh85311@gmail.com>
2023-08-28 08:04:04 +0900

    정해진 형식으로 시간을 입력 받아서 출력하기 (서식지정자)

---
+++ src/First/basic2.java
@@ -0,0 +1,12 @@
+package First;
+//정해진 형식으로 시간을 입력 받아서 출력하기 (서식지정자)
+
+import java.util.Scanner;
+
+//20:3:5 입력시 오후8시03분05초로 출력하도록 scanf를 통한 입력 포맷을 만드는 방법을 확인 한다.
+public class basic2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
