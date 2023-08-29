a12b4076728b6ffebe6e11bc03808a61491c2dee
lamsasa <dh85311@gmail.com>
2023-08-30 02:21:39 +0900

    입력 받은 n개의 원소에 대한 평균 구하기

---
+++ src/First/basic3.java
@@ -0,0 +1,11 @@
+package First;
+
+import java.util.Scanner;
+
+//첫번째 열에 입력 받을 숫자의 개수 :
+//두번째 열에 공백 기준으로 첫번째 열에서 주어진 개수 만큼 성적 입력 (단 성적은 정수)
+public class basic3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
