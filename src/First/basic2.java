655c9240c80df3112c3083f91b78b27b465e270e
lamsasa <dh85311@gmail.com>
2023-08-30 02:20:08 +0900

    정해진 형식으로 시간을 입력 받아서 출력하기 (서식지정자)

--- src/First/basic2.java
+++ src/First/basic2.java
@@ -7,6 +7,18 @@ import java.util.Scanner;
 public class basic2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("시간 입력 : ");
+        String times = sc.next();
+        String[] splitTime = times.split(":");
+        int intHour = Integer.parseInt(splitTime[0]);
+        int intMin = Integer.parseInt(splitTime[1]);
+        int intSec = Integer.parseInt(splitTime[2]);
 
+        if(intHour > 11) {
+            intHour -= 12;
+            System.out.printf("오후 %02d시 %02d분 %02d초\n", intHour, intMin, intSec);
+        } else {
+            System.out.printf("오전 %02d시 %02d분 %02d초\n", intHour, intMin, intSec);
+        }
     }
 }
