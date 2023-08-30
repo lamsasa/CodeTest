686f8c7dede1d16856464564625fbee47c3f2e87
lamsasa <dh85311@gmail.com>
2023-08-30 23:35:19 +0900

    이름, 주소, 전화번호, 성별을 입력 받아 출력 하기

---
+++ src/First/basic4.java
@@ -0,0 +1,8 @@
+package First;
+//두개의 숫자를 입력받아 스왑하기
+
+public class basic4 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/First/basic5.java
@@ -0,0 +1,4 @@
+package First;
+//이름, 주소, 전화번호, 성별을 입력 받아 출력 하기
+public class basic5 {
+}
