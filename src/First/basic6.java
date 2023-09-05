ba744dbd6bb2b6ef705979f8e585d03463105751
lamsasa <dh85311@gmail.com>
2023-09-05 21:14:48 +0900

    공백이 있는 문자열 입력받아 출력 하기

--- src/First/basic6.java
+++ src/First/basic6.java
@@ -1,7 +1,14 @@
 package First;
+
+import java.util.Scanner;
+
 // [1]서울, [2]경기, [3]인천, [4]수원 으로 입력 받아 문자열로 출력 하기
 public class basic6 {
     public static void main(String[] args) {
-
+        String[] city = {"", "서울", "경기", "인천", "수원"};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("도시를 선택 하세요. [1]서울, [2]경기, [3]인천, [4]수원 : ");
+        int num = sc.nextInt();
+        System.out.println(city[num]);
     }
 }
---
+++ src/First/basic7.java
@@ -0,0 +1,4 @@
+package First;
+//공백이 있는 문자열 입력받아 출력 하기
+public class basic7 {
+}
