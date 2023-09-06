d9603f60c724ae6a43697812412d175364377067
lamsasa <dh85311@gmail.com>
2023-09-06 20:30:07 +0900

    공백이 있는 문자열 입력받아 출력 하기

--- src/First/basic7.java
+++ src/First/basic7.java
@@ -1,4 +1,14 @@
 package First;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 //공백이 있는 문자열 입력받아 출력 하기
 public class basic7 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String test = br.readLine();
+        System.out.println(test);
+    }
 }
