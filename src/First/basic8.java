60535f7b7e5d709e05586934d9e644bf54b50bd0
lamsasa <dh85311@gmail.com>
2023-09-09 02:15:01 +0900

    정수 n을 입력 받아 n * n 출력 하기

--- src/First/basic8.java
+++ src/First/basic8.java
@@ -1,5 +1,16 @@
 package First;
 //정수 n을 입력 받아 n * n 출력 하기
 
+import java.util.Scanner;
+
 public class basic8 {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("정수 입력 : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n * n; i++) {
+            System.out.printf("%4d", i);
+            if (i % n == 0) System.out.println();
+        }
+    }
 }
