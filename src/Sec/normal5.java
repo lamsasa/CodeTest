3fbaaa862253739b81917874cc1699cd8fc7fd12
lamsasa <dh85311@gmail.com>
2023-09-20 22:31:48 +0900

    7개의 수를 입력 받아 홀수와 짝수를 나누어서 출력

--- src/Sec/normal5.java
+++ src/Sec/normal5.java
@@ -1,4 +1,13 @@
 package Sec;
 // 7개의 수를 입력 받아 홀수와 짝수를 나누어서 출력
+
+// 7개의 정수를 입력 받아 홀수와 짝수를 각 배열에 나주어 저장 합니다.
+//
+//기존의 배열과 vector 구조의 차이를 이해 합니다.
+//
+//갯수 제한 없도록 수정 합니다.
 public class normal5 {
+    public static void main(String[] args) {
+
+    }
 }
