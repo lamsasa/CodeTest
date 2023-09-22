a9e66cd9d7d27f97c41e63f7836d9ba9795c41e6
lamsasa <dh85311@gmail.com>
2023-09-23 01:54:45 +0900

    문자열 추가하기

--- src/Sec/normal6.java
+++ src/Sec/normal6.java
@@ -1,4 +1,14 @@
 package Sec;
 //문자열 추가하기
+
+//2개의 문자열을 포인터 변수 s와 k에, 양의 정수를 정수형 변수 n에 각가 전달 받아 s 문자열의 뒤 부분의 n개 문자를 k문자열 앞에 끼워 넣는 코드 작성
+//
+//예)  s :  seoul
+//
+//       k :  korea
+//
+//       n :  2
+//
+//       result : ulkorea
 public class normal6 {
 }
