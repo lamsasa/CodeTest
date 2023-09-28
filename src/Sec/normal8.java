2dd2093d03227fc1c10464fc23d09d56a9a4e8bc
lamsasa <dh85311@gmail.com>
2023-09-28 19:17:16 +0900

    입력 받은 수가 소수인지 아닌지 판별하기

--- src/Sec/normal8.java
+++ src/Sec/normal8.java
@@ -1,4 +1,22 @@
 package Sec;
 // 입력 받은 수가 소수인지 아닌지 판별하기
+
+import java.util.Scanner;
+
+//- 1과 자기자신이외에는 나누어지지 않는 수
 public class normal8 {
+    public static void main(String[] args) {
+        int n;
+        System.out.print("정수를 입력 하세요 : ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        System.out.println("소수 : " + func(n));
+    }
+
+    static boolean func(int n) {
+        for (int i = 2; i < n; i++) {
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
 }
