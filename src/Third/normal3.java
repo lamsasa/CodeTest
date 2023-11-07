51865308ad72fe0cd804225f1cb9e65fa2ad3dc1
lamsasa <dh85311@gmail.com>
2023-11-08 03:59:13 +0900

    X보다 작은 수

--- src/Third/normal3.java
+++ src/Third/normal3.java
@@ -1,6 +1,8 @@
 package Third;
 // X보다 작은 수
 
+import java.util.Scanner;
+
 // ### [문제]
 //
 //정수 N개로 이루어진 배열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
@@ -16,6 +18,14 @@ package Third;
 //X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다. X보다 작은 수는 적어도 하나 존재한다.
 public class normal3 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] < x) System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
