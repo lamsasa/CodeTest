88100df2e063a6a93d8dc59113ec04cfb60f1feb
lamsasa <dh85311@gmail.com>
2023-11-10 18:45:36 +0900

    평균은 넘겠지?

--- src/Third/normal4.java
+++ src/Third/normal4.java
@@ -1,7 +1,33 @@
 package Third;
+
+import java.util.Scanner;
+
 // 평균은 넘겠지?
 public class normal4 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int testCnt = sc.nextInt();
+        double[]result = new double[testCnt];
+        for(int i = 0; i < result.length; i++) {
+            result[i] = overRate();
+        }
+        for(double val : result) {
+            System.out.printf("%.3f%%\n", val);
+        }
+    }
+    static double overRate() {
+        int total = 0, overCnt = 0;
+        Scanner sc = new Scanner(System.in);
+        int studentCnt = sc.nextInt();
+        int[] score = new int[studentCnt];
+        for(int i = 0; i < score.length; i++) {
+            score[i] = sc.nextInt();
+            total += score[i];
+        }
+        double average = (double)total / studentCnt;
+        for(int val : score) {
+            if (val > average) overCnt++;
+        }
+        return (double)overCnt/studentCnt * 100;
     }
 }
