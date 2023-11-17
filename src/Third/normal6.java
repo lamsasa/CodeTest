61a34cadd67b388beb68583ba622f750851dbc6e
lamsasa <dh85311@gmail.com>
2023-11-18 01:34:22 +0900

    피타고라스 정리

--- src/Third/normal6.java
+++ src/Third/normal6.java
@@ -1,5 +1,28 @@
 package Third;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 // 피타고라스 정리
 public class normal6 {
-
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        List<String> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            int sum = 0;
+            for(int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+                sum += arr[i];
+            }
+            if(sum == 0) break;
+            Arrays.sort(arr);
+            if((arr[2] * arr[2]) == (arr[0] * arr[0]) + (arr[1] * arr[1])) {
+                list.add("right");
+            } else list.add("wrong");
+        }
+        for(String e : list) System.out.println(e);
+    }
 }
