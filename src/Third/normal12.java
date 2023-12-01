108911215c001c4fff2f195ccaf1e5dc980742be
lamsasa <dh85311@gmail.com>
2023-12-02 02:30:10 +0900

    전자레인지

--- src/Third/normal12.java
+++ src/Third/normal12.java
@@ -1,4 +1,7 @@
 package Third;
+
+import java.util.Scanner;
+
 // 전자레인지
 // ## 문제
 //
@@ -18,4 +21,17 @@ package Third;
 //
 //여러분은 T초를 위한 최소버튼 조작의 A B C 횟수를 첫 줄에 차례대로 출력해야 한다. 각각의 횟수 사이에는 빈 칸을 둔다. 해당 버튼을 누르지 않는 경우에는 숫자 0을 출력해야한다. 만일 제시된 3개의 버튼으로 T초를 맞출 수 없으면 음수 -1을 첫 줄에 출력해야 한다.
 public class normal12 {
+    public static void main(String[] args) {
+        int t, a, b, c;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("요리시간(초) : ");
+        t = sc.nextInt();
+        if(t % 10 != 0) System.out.println("-1");
+        else {
+            a = t / 300;
+            b = t % 300 / 60;
+            c = t % 300 % 60 / 10;
+            System.out.printf("%d %d %d\n", a, b, c);
+        }
+    }
 }
