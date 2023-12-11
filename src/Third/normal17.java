a42db629961720eeb6b5d6846d9ff5805b5e6aca
lamsasa <dh85311@gmail.com>
2023-12-11 16:14:54 +0900

    더하기 싸이클

--- src/Third/normal17.java
+++ src/Third/normal17.java
@@ -1,4 +1,7 @@
 package Third;
+
+import java.util.Scanner;
+
 // 더하기 싸이클
 // ## 문제
 //
@@ -18,4 +21,21 @@ package Third;
 //
 //첫째 줄에 N의 사이클 길이를 출력한다.
 public class normal17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sumNumber = 0; // 입력받은 수의 10의 자리와 1자리를 분리해서 더함
+        int newNumber = 0; // 새롭게 만들어진 수
+        int cnt = 0; // 사이클 회수 누적
+        System.out.print("입력 : ");
+        int input = sc.nextInt();
+        int num = input; // 입력 받은 값이 변경되는 걸 막기 위해 임시 값을 만듬
+        while (true) {
+            sumNumber = (num / 10) + (num % 10); // 입력값을 10의 자리와 1자리로 분리해서 더함
+            newNumber = (num % 10) * 10 + (sumNumber % 10); // 입력값의 1의 자리 + 계산된 값의 1자의 더함
+            cnt++;
+            if(newNumber == input) break; // 더하기 조건에 따라 1 Cycle 순회 후 입력 받은 값과 비교
+            num = newNumber; // 순회를 위해 만들어진 값을 순회용 변수에 대입
+        }
+        System.out.println(cnt);
+    }
 }
