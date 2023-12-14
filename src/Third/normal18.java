7793e301c4a8b5645bed33ac769a5fed58240b56
lamsasa <dh85311@gmail.com>
2023-12-15 07:43:43 +0900

    한번에 정수 여러개 입력 받아서 합계 구하기 (입출력과 반복문)

--- src/Third/normal18.java
+++ src/Third/normal18.java
@@ -1,4 +1,7 @@
 package Third;
+
+import java.util.Scanner;
+
 // 한번에 정수 여러개 입력 받아서 합계 구하기 (입출력과 반복문)
 // **정수의 개수을 입력 받고 정수의 개수 만큼의 값을 입력 받아 총 합을 구하는 문제 입니다.**
 //
@@ -7,5 +10,16 @@ package Third;
 //- 반복문의 기본적인 사용 방법을 복습 한다.
 //- 합계를 구하기 위해서는 추가적인 변수가 필요하고 반드시 초기화 되어 있어야 한다.
 public class normal18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("정수의 갯수를 입력 하세요 : ");
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int tmp = sc.nextInt();
+            sum += tmp;
+        }
+        System.out.println("sum : " + sum);
+    }
 
 }
