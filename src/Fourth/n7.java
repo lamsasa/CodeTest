383dcf222833a1c9fb5073b87d305f4e277cf26e
lamsasa <dh85311@gmail.com>
2023-12-31 04:38:42 +0900

    알람 설정하기

--- src/Fourth/n7.java
+++ src/Fourth/n7.java
@@ -1,4 +1,20 @@
 package Fourth;
+
+import java.util.Scanner;
+
 //알람 설정하기
 public class n7 {
+    public static void main(String[] args)  {
+        int hour, min, calc;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("시간 입력 : ");
+        hour = sc.nextInt();
+        min = sc.nextInt();
+        calc = (hour * 60) + min;
+        if (calc < 45) {
+            calc = (24 * 60) + min;
+        }
+        calc -= 45;
+        System.out.printf("%d %d\n", (calc / 60), (calc % 60));
+    }
 }
