410b67d96f8c611700905a2d18f29b627497da06
lamsasa <dh85311@gmail.com>
2024-01-11 05:00:08 +0900

    7개의 수를 입력 받아 홀수와 짝수를 나누어서 출력

--- src/Fifth/n4.java
+++ src/Fifth/n4.java
@@ -1,4 +1,7 @@
 package Fifth;
+
+import java.util.Scanner;
+
 // 7개의 수를 입력 받아 홀수와 짝수를 나누어서 출력
 // 7개의 정수를 입력 받아 홀수와 짝수를 각 배열에 나주어 저장 합니다.
 //
@@ -6,5 +9,38 @@ package Fifth;
 //
 //갯수 제한 없도록 수정 합니다.
 public class n4 {
-
+    public static void main(String[] args) {
+        Calc_func calc_func = new Calc_func();
+        calc_func.inputInt();
+        calc_func.oddEven();
+        calc_func.dispOutput();
+    }
+}
+class Calc_func {
+    int[] arr = new int[7];
+    int[] even = new int[7];
+    int[] odd = new int[7];
+    int e_idx = 0;
+    int o_idx = 0;
+    public void inputInt() {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 7; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+    public void oddEven() {
+        for (int i : arr) {
+            if (i % 2 == 0)  even[e_idx++] = i;
+            else odd[o_idx++] = i;
+        }
+    }
+    public void dispOutput() {
+        System.out.println("홀수 : ");
+        for (int i = 0; i < o_idx; i++) System.out.printf("%2d", odd[i]);
+        System.out.println();
+        System.out.println("짝수 : ");
+        for (int i = 0; i < e_idx; i++) System.out.printf("%2d", even[i]);
+        System.out.println();
+    }
+    }
 }
