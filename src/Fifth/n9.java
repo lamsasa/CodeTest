f15f9f85662de59b16a5252273f57e69572d4621
lamsasa <dh85311@gmail.com>
2024-01-24 01:56:03 +0900

    별 찍기

--- src/Fifth/n9.java
+++ src/Fifth/n9.java
@@ -1,7 +1,15 @@
 package Fifth;
+
+import java.util.Scanner;
+
 // 별 찍기
 public class n9 {
     public static void main(String[] args) {
-
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
