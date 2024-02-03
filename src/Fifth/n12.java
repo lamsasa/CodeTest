e1620a19f2cc794d96f900e31e47a2ee2750d017
lamsasa <dh85311@gmail.com>
2024-02-04 01:52:33 +0900

    7자리 로또 번호 만들기

--- src/Fifth/n12.java
+++ src/Fifth/n12.java
@@ -1,4 +1,12 @@
 package Fifth;
 // 7자리 로또 번호 만들기
 public class n12 {
+    public static void main(String[] args) {
+        int[] lotto = new int[7];
+        for(int i = 0; i < 6; i++) {
+            lotto[i] = (int)(Math.random() * 51);
+        }
+        lotto[6] = (int)(Math.random() * 51) + 50;
+        for(int e : lotto) System.out.print(e + " ");
+    }
 }
