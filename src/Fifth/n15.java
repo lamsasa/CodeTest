9cc0bd0cfe4ed2fbbd85747afff04818f55fbc7d
lamsasa <dh85311@gmail.com>
2024-02-10 00:12:34 +0900

    문자열 비교

--- src/Fifth/n15.java
+++ src/Fifth/n15.java
@@ -1,4 +1,21 @@
 package Fifth;
+
+import java.util.Scanner;
+
 // 문자열 비교
-public class n15 {
+public class n15 {public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String a = sc.next();
+    String b = sc.next();
+    int cnt = 0;
+    for (int i = 0; i < a.length(); i++) {
+        if(a.charAt(i) == b.charAt(i)) {
+            System.out.print(a.charAt(i));
+            cnt++;
+        } else break;
+    }
+    System.out.println();
+    if(cnt != 0) System.out.println(cnt);
+    else System.out.println("No CS");
+}
 }
