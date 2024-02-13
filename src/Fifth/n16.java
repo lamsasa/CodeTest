41d6f4fc77ff8571f261172f5eb07de01350d1c1
lamsasa <dh85311@gmail.com>
2024-02-14 08:18:56 +0900

    문자열 반전 문제

--- src/Fifth/n16.java
+++ src/Fifth/n16.java
@@ -1,4 +1,7 @@
 package Fifth;
+
+import java.util.Scanner;
+
 // 문자열 반전
 //
 //문자열을 입력으로 사용
@@ -7,4 +10,13 @@ package Fifth;
 //
 //역함수를 만들어야 한다.
 public class n16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("문자열을 입력 : ");
+        String input = sc.next();
+        for(int i = input.length()-1; i >= 0; i--) {
+            System.out.print(input.charAt(i));
+        }
+        System.out.println();
+    }
 }
