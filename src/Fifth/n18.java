93e94666cb13a8a8be13954054a90cf305c14695
lamsasa <dh85311@gmail.com>
2024-02-22 02:17:32 +0900

    핸드폰 요금

--- src/Fifth/n18.java
+++ src/Fifth/n18.java
@@ -1,4 +1,7 @@
 package Fifth;
+
+import java.util.Scanner;
+
 //핸드폰 요금
 //## 문제
 //
@@ -23,4 +26,26 @@ package Fifth;
 //
 //영식은 Y로, 민식은 M으로 출력한다.
 public class n18 {
+    public static void main(String[] args) {
+        int[] call = new int[20];
+        int yPay = 0, mPay = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("통화 횟수 입력 : ");
+        int n = sc.nextInt();
+        System.out.println("통화 시간 입력 : ");
+        for(int i = 0; i < n; i++) {
+            call[i] = sc.nextInt();
+        }
+        for(int i = 0; i < n; i++) {
+            yPay += (call[i] / 30) * 10 + 10;
+            mPay += (call[i] / 60) * 15 + 15;
+        }
+        if(yPay > mPay) {
+            System.out.println("M " + mPay);
+        } else if(yPay < mPay) {
+            System.out.println("Y " + yPay);
+        } else {
+            System.out.println("Y M " + yPay);
+        }
+    }
 }
